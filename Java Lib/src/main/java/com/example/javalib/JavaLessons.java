0f0bc2ac4ed33897451be4ac37bb1673117f2287
JIAWEI68 <ang.jiawei56@gmail.com>
2021-04-22 19:11:33 +0800

    Java Library data for school

---
+++ Java Lib/src/main/java/com/example/javalib/JavaLessons.java
@@ -0,0 +1,23 @@
+package com.example.javalib;
+
+public class JavaLessons {
+    public static void main(String[] args){
+     String price1 = "12.90";
+        System.out.println("price1 + price1 = " + price1+price1);
+        double lengthOfTable = 1.6;
+        String Flower = "Angelica";
+        double priceOfIphone = 1199.99;
+        int Age = 17;
+        char Grade = 'B';
+        boolean RainStopped = true;
+        System.out.println("Age + lengthOfTable = " + Age+lengthOfTable);
+        String name = "John";
+        System.out.println("Hello world!");
+        System.out.println("name + lengthOfTable = " + name+lengthOfTable);
+        System.out.println("priceOfIphone + Grade = " + priceOfIphone+Grade);
+        System.out.println("Age + Age = " + Age+Age);
+
+
+
+    }
+}
